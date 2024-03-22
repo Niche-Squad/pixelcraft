da7e3689f578cb7ac72350e5e24f03bedc6b4651
poissonfish <niche@vt.edu>
2024-03-22 17:32:39 -0400

    improve performance

--- globals.py
+++ globals.py
@@ -1,4 +1,14 @@
 import streamlit as st
+from PIL import Image
+
+
+def load_image(img, tgt_width=640):
+    pil_in = Image.open(img)
+    w, h = pil_in.size
+    ratio = h / w
+    height = int(tgt_width * ratio)
+    pil_out = pil_in.resize((tgt_width, height))
+    return pil_out
 
 
 def init_globals():
@@ -30,22 +40,42 @@ def init_globals():
 def update_globals():
     n_imgs = st.session_state.n_imgs
     loaded = st.session_state.loaded
-    if n_imgs > 0 and not loaded:
-        st.session_state.loaded = True
-        st.session_state.json_data = [None for _ in range(n_imgs)]
-        st.session_state.json_out = [
-            {
-                "filename": "unknown",
-                "objects": [],
-            }
-            for _ in range(n_imgs)
-        ]
-        st.session_state.cropped_imgs = [None for _ in range(n_imgs)]
-        st.session_state.pil_imgs = [None for _ in range(n_imgs)]
+    if n_imgs > 0:
+        if not loaded:
+            st.session_state.loaded = True
+            st.session_state.json_data = [None for _ in range(n_imgs)]
+            st.session_state.json_out = [
+                {
+                    "filename": "unknown",
+                    "objects": [],
+                }
+                for _ in range(n_imgs)
+            ]
+            st.session_state.pil_imgs = [None for _ in range(n_imgs)]
+            st.session_state.cropped_imgs = [None for _ in range(n_imgs)]
+        else:
+            # if uploading more images later
+            if len(st.session_state.json_data) < n_imgs:
+                n_extra = n_imgs - len(st.session_state.json_data)
+                st.session_state.json_data += [None for _ in range(n_extra)]
+                st.session_state.json_out += [None for _ in range(n_extra)]
+                st.session_state.pil_imgs += [None for _ in range(n_extra)]
+                st.session_state.cropped_imgs += [None for _ in range(n_extra)]
+        caching_images()
 
-    # # if uploading more images later
-    # if len(st.session_state.json_data) < n_imgs:
-    #     n_extra = n_imgs - len(st.session_state.json_data)
-    #     st.session_state.json_data += [None for _ in range(n_extra)]
-    #     st.session_state.json_out += [None for _ in range(n_extra)]
-    #     st.session_state.cropped_imgs += [None for _ in range(n_extra)]
+
+def caching_images():
+    text_bar = "Caching images..."
+    bar = st.progress(0, text=text_bar)
+    n_imgs = st.session_state.n_imgs
+    file_imgs = st.session_state.file_imgs
+    pil_imgs = st.session_state.pil_imgs
+    portion = int(100 / n_imgs)
+    # showing progress bar of caching images
+    for i in range(n_imgs):
+        pil = pil_imgs[i]
+        if pil is None:
+            pil = load_image(file_imgs[i])
+            st.session_state.pil_imgs[i] = pil
+        bar.progress((i + 1) * portion, text=text_bar)
+    bar.empty()
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import streamlit as st
+from streamlit_drawable_canvas import st_canvas
+
 
 # local imports
 from sidebar import deploy_sidebar
 from globals import init_globals, update_globals
 from widgets import (
-    drawable_image,
     image_uploader,
     next_prev_buttons,
     slider_selector,
     chart_anns,
+    toggle_canvas,
+    Timer,
 )
 from outputs import make_dataframe, extract_canvas
 from callbacks import enable_hotkeys
@@ -28,8 +31,8 @@ def main():
     # components
     st.title("PixelCraft")
     ## --- Image Uploader ---
-    file_imgs = image_uploader()
-    st.session_state.n_imgs = len(file_imgs)
+    st.session_state.file_imgs = image_uploader()
+    st.session_state.n_imgs = len(st.session_state.file_imgs)
     update_globals()
     loaded = st.session_state.loaded
     if not loaded:
@@ -37,37 +40,47 @@ def main():
     else:
         ## --- Image Viewer ---
         st.subheader("Image Viewer")
-        edit = st.toggle("Edit Bounding Boxes", False)
-
+        tog_edit, tog_auto = toggle_canvas()
         cur_i = st.session_state.cur_i
-        canvas, img_pil = drawable_image(
-            idx=st.session_state.cur_i,
-            img=file_imgs[cur_i],
-            state=st.session_state.json_data[cur_i],
-            is_transform=edit,
+        img_pil = st.session_state.pil_imgs[cur_i]
+        state = st.session_state.json_data[cur_i]
+        filename = st.session_state.file_imgs[cur_i].name
+        # drawing state
+        canvas = st_canvas(
+            initial_drawing=state,
+            fill_color="rgba(255, 90, 0, 0.3)",
+            stroke_width=1,
+            stroke_color="rgb(255, 0, 0, 1)",
+            background_color="#fff",
+            background_image=img_pil,
+            update_streamlit=tog_auto,
+            height=img_pil.height,
+            width=img_pil.width,
+            drawing_mode="transform" if tog_edit else "rect",
+            # point_display_radius=0, # not available in 0.8.0
+            key="canvas_%d" % cur_i,
         )
-        st.write(file_imgs[cur_i].name)
+        st.write(filename)
         # extract canvas
         while not canvas.json_data:
             pass
+
+        # update session state
         json_objs, cropped_img = extract_canvas(
             canvas.json_data["objects"],
             img_pil,
         )
         json_out = dict(
             {
-                "filename": file_imgs[cur_i].name,
+                "filename": filename,
                 "objects": json_objs,
             }
         )
-        # update session state
         st.session_state.json_data_tmp = canvas.json_data
         st.session_state.json_out[cur_i] = json_out
         st.session_state.cropped_imgs[cur_i] = cropped_img
-        st.session_state.pil_imgs[cur_i] = img_pil
 
         ## --- Image Selector ---
-        st.subheader("Image Selector")
         next_prev_buttons()
         slider_selector()
         chart_anns()
